1f54b92b475f4ed5413c45e3f84d4b334a4c221e
Tomiţă Ştefan <tomstef96@yahoo.com>
2022-06-13 13:18:04 +0300

    new commit

--- Assignments/Assing11-Main JavaScript Project/script.js
+++ Assignments/Assing11-Main JavaScript Project/script.js
@@ -1,53 +1,81 @@
-function computerPlay() {
-   const pick = ['rock', 'paper', 'scissors'];
-   return pick[Math.floor(Math.random() * pick.length)];
-}
-
-function playRound(playerSelection, computerSelection) {
-   if (playerSelection === computerSelection) {
-      return `It's a tie! you both picked ${playerSelection}`;
-   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      return "You win! Rock beats Scissors";
-   } else if (playerSelection === "paper" && computerSelection === "rock") {
-      return "You win! Paper beats Rock";
-   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-      return "You win! Scissors beats Paper";
+const computerPlay = () => {
+   const choices = [`Rock`, `Paper`, `Scissors`];
+   const selectedChoice = Math.floor(Math.random() * 3);
+ 
+   return choices[selectedChoice];
+ }
+ 
+ const whoWins = (player, computer) => {
+   switch (player){
+     case computer:
+       return {'msg': `It's a tie!`, 'winner': `tie`};
+       break;
+     case 'ROCK':
+       if (computer === `SCISSORS`){
+         return {'msg': `You Win! Rock beats Scissors`, 'winner': `player`};
+       } else {
+         return {'msg': `You Lose! Paper beats Rock`, 'winner': `computer`};
+       }
+       break;
+     case 'PAPER':
+       if (computer === `ROCK`){
+         return {'msg': `You Win! Paper beats Rock`, 'winner': `player`};
+       } else {
+         return {'msg': `You Lose! Scissors beats Paper`, 'winner': `computer`};
+       }
+       break;
+     case 'SCISSORS':
+       if (computer === `PAPER`){
+         return {'msg': `You Win! Scissors beats Paper`, 'winner': `player`};
+       } else {
+         return {'msg': `You Lose! Rock beats Scissors`, 'winner': `computer`};
+       }
+       break;
+     default:
+       return [``,`computer`];
+       break;
+   }
+ }
+ 
+ const playJankenRound = (playerChoice, computerChoice) => {
+   const player = playerChoice.toUpperCase();
+   const computer = computerChoice.toUpperCase();
+ 
+   return whoWins(player,computer);
+ }
+ 
+ 
+ const game = () => {
+   let score = {
+     'player': 0, 
+     'computer': 0,
+     'tie': 0,
+   };
+   
+   for (let i = 0; i < 5; i++){
+ 
+     let playerChoice = prompt(`Please insert either rock, paper or scissors`);
+     while (playerChoice.toLowerCase() !== 'rock' && playerChoice.toLowerCase() !== 'paper' && playerChoice.toLowerCase() !== 'scissors'){
+       playerChoice = prompt(`Player inserted invalid choice. Please select between rock, paper or scissors`);
+     }
+ 
+     let result = playJankenRound(playerChoice,computerPlay());
+     alert(`This round... ${result.msg}`);
+     console.log(result.msg);
+     score[result.winner]++;
+   }
+ 
+   console.log(score);
+   if (score.player == score.computer){
+     alert(`It's a tie! \nThe result was: \nPlayer: ${score.player} \nComputer: ${score.computer} \nTies: ${score.tie}`);
+   } else if (score.player > score.computer){
+     alert(`You Win! \nThe result was: \nPlayer: ${score.player} \nComputer: ${score.computer} \nTies: ${score.tie}`);
    } else {
-      return `You lose! ${computerSelection} beats ${playerSelection}`;
+     alert(`You Lose! \nThe result was: \nPlayer: ${score.player} \nComputer: ${score.computer} \nTies: ${score.tie}`);
    }
-}
-
-
-const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
-
-const result = {win:0, tie:0, loss:0};
-const win = "You win";
-const tie = "tie";
-const loss = "You lose";
-
-function game() {
-   for (i = 0; i < 5; i++) {
-      console.log(playRound(playerSelection, computerSelection));
-      if (playRound() === win) {
-         result.win++;
-      } else if (playRound() == tie) {
-         result.tie++;
-      } else {
-         result.loss++;
-      }
-   }
-
-   console.log("Wins: " + result.win);
-   console.log("Losses: " + result.loss);
-   console.log("Ties: " + result.tie);
-}
-
-game();
-
-
-
-//https://stackoverflow.com/questions/63655685/struggling-to-loop-through-function-and-return-proper-results-for-odin-project-r
-//https://stackoverflow.com/questions/63655685/struggling-to-loop-through-function-and-return-proper-results-for-odin-project-r
-// https://codepen.io/christianmmason/pen/ErxZeE
+ }
+ 
+ //Uncomment next line if you want it to play automatically
+ game(); 
+ 
+ 
---
+++ test/script.js
@@ -0,0 +1,13 @@
+// const container = document.querySelector('.container');
+
+// const paragraph = document.createElement('p');
+// paragraph.innerText = "Hei I'm red";
+// paragraph.style.color = "red";
+
+// const hThree = document.createElement('h3');
+// hThree.innerHTML = 'background: pink; border: 3px solid black';
+// div.innerHTML = "<h1> I'm in a div</h1> <p>ME TOO!</p>";
+
+// container.append(paragraph, hThree, div);
+
+
